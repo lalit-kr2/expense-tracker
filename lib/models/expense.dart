2d68fda406bbce92c410d035d1f3c115dba8a44a
lalit-kr2 <lalit.kr1009@gmail.com>
2025-07-14 20:53:07 +0530

    first commit

---
+++ lib/models/expense.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat().add_yMd();
+
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining_sharp,
+  Category.travel: Icons.train_sharp,
+  Category.leisure: Icons.movie_sharp,
+  Category.work: Icons.work_sharp
+};
+
+class Expense {
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v1();
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,50 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: "Flutter Course",
+        amount: 4599.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: "Movie",
+        amount: 780.79,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+        actions: [
+          IconButton(onPressed: _openAddExpenseOverlay, icon: Icon(Icons.add))
+        ],
+      ),
+      body: Column(
+        children: [
+          Text("The Chart..."),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
